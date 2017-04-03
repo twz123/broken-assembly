5a8adf1d769f926a8361866d0fd2e0de30b3174f
Tom Wieczorek <t.wieczorek@yieldlab.de>
2017-04-03 22:40:50 +0200

    Downgrade to Java 7 and enable Travis build matrix to test different JDKs.

--- src/test/java/com/github/twz123/brokenassembly/BrokenAssemblyIT.java
+++ src/test/java/com/github/twz123/brokenassembly/BrokenAssemblyIT.java
@@ -5,7 +5,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Optional;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -18,12 +17,8 @@ public class BrokenAssemblyIT {
 
     @Test
     public void unjarBrokenAssembly() throws Exception {
-        final String[] command = {
-                Optional.of(System.getenv("JAVA_HOME")) //
-                        .map(javaHome -> Paths.get(javaHome, "bin", "jar").toString()) //
-                        .orElse("jar"), //
-                "xf", //
-                System.getenv("BROKEN_ASSEMBLY_JAR") //
+        final String[] command = { //
+                pathToJarBinary(), "xf", System.getenv("BROKEN_ASSEMBLY_JAR") //
         };
 
         System.out.println("Executing " + Arrays.toString(command));
@@ -36,5 +31,10 @@ public class BrokenAssemblyIT {
         final Process unjar = unjarTemplate.start();
 
         assertThat("Non-zero exit code from jar executable.", unjar.waitFor(), is(0));
+    }
+
+    private String pathToJarBinary() {
+        final String javaHome = System.getenv("JAVA_HOME");
+        return javaHome == null ? "jar" : Paths.get(javaHome, "bin", "jar").toString();
     }
 }
